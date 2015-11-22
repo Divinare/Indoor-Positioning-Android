e72138c29921eb50333ae062aa861e06a769ab21
Joe <joe@Joes-MacBook-Pro.local>
2015-11-22 09:52:42 +0200

    all works

---
+++ app/src/main/java/joe/indoorlocalization/LocateActivity.java
@@ -0,0 +1,14 @@
+package joe.indoorlocalization;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class LocateActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locate);
+    }
+
+}

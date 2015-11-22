33352cfb522181c97ce12284810859cc27f3b63e
Joe <joe@Joes-MacBook-Pro.local>
2015-11-22 13:10:55 +0200

    imageview pinch zoom works

--- app/src/main/java/joe/indoorlocalization/CalibrationActivity.java
+++ app/src/main/java/joe/indoorlocalization/CalibrationActivity.java
@@ -1,13 +1,16 @@
 package joe.indoorlocalization;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class CalibrationActivity extends AppCompatActivity {
+
+    static String TAG = CalibrationActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,8 @@ public class CalibrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calibration);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        CustomImageView mImageView = (CustomImageView)findViewById(R.id.customImageView2);
+        Bitmap exatum1 = BitmapFactory.decodeResource(getResources(), R.drawable.exactum1);
+        mImageView.setBitmap(exatum1);
     }
-
 }
--- app/src/main/java/joe/indoorlocalization/MainActivity.java
+++ app/src/main/java/joe/indoorlocalization/MainActivity.java
@@ -12,20 +12,22 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     static String TAG = "MainActivity";
-
+   // final Intent intentSetPosition = new Intent(this, CalibrationActivity.class);
+   // final Intent intentHelp = new Intent(this, CalibrationActivity.class);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "The app is running :)))");
-        final Intent intent = new Intent(this, CalibrationActivity.class);
 
+
+        final Intent intentCalibrate = new Intent(this, CalibrationActivity.class);
 
         Button btnSaveScan = (Button)(findViewById(R.id.btnMenuCalibrate));
         btnSaveScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(intent);
+                startActivity(intentCalibrate);
                 Log.i(TAG, "Button presseeed");
             }
         });
@@ -44,8 +46,19 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+        final Intent intentLocate = new Intent(this, LocateActivity.class);
+        final Intent intentCalibrate = new Intent(this, CalibrationActivity.class);
+
+
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_locate) {
+            startActivity(intentLocate);
+        } else if(id == R.id.menu_calibrate) {
+            startActivity(intentCalibrate);
+        } else if(id == R.id.menu_setPosition) {
+            return true;
+        } else if(id == R.id.menu_help) {
             return true;
         }
         return super.onOptionsItemSelected(item);
